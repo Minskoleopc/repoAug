9834b7df52052016202e1418da967840041e7778
unknown <Minskoleopc>
2024-09-16 19:21:33 +0530

    added

---
+++ Revision/script1.js
@@ -0,0 +1,59 @@
+// loops in javascript
+// program 1 
+// 1 to 5
+
+console.log(1)
+console.log(2)
+console.log(3)
+console.log(4)
+console.log(5)
+
+// program 2   ----> for and while loop
+
+// for(initialzation ; condition ; increment/decrement){
+//     // statements
+// }
+
+// program 1 to 3
+for(let i = 1 ; i <= 3 ; i++){ // 2 // 3
+    console.log(i) // 1 // 2 // 3
+}
+// 1 <= 3  -----> true
+// 2 <= 3  -----> true
+// 3 <= 3  -----> true
+
+
+// program 2
+// print 1 to 5
+for(let i = 1 ; i <= 5 ; i++){ // 2 // 3 // 4 // 5 // 6
+    console.log(i) // 1 // 2 // 3 // 4 // 5
+}
+
+// program 3
+// print 5 to 1
+for(let i = 5 ; i >= 1 ; i--){ // 4 // 3 // 2 // 1 // 0
+    console.log(i) // 5 // 4 // 3 // 2 // 1
+}
+
+// program 4
+// print table of 2
+for(let i = 2 ; i <= 20 ; i = i + 2){ // 4 // 6 // 8 // 10 --- 22
+    console.log(i) // 2 // 4 // 6 // 8 // 10 ------ 20
+}
+
+// program 5
+// table of 5 in reverse 
+for(let i = 50 ; i >= 5 ; i = i- 5){ // 45 // 40 // 35 ----- 0
+    console.log(i) // 50 // 45 // 40 -------- 5
+}
+
+
+
+
+
+
+
+
+
+
+
