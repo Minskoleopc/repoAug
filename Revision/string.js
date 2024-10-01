708c0cd7ef962a7500d5272c83e119ecc75536e5
unknown <Minskoleopc>
2024-10-01 19:24:09 +0530

    added

--- Revision/string.js
+++ Revision/string.js
@@ -90,6 +90,92 @@ while (i2 < city2.length){
 }
 
 // String - methods 
+// toUpperCase()
+let city1 = "pune"  // PUNE
+let q1 = city1.toUpperCase()
+console.log(q1)
+
+
+// toLowerCase()
+let city3 = "NAGPUR"
+let q2 = city3.toLowerCase()
+console.log(q2)
+
+
+// includes()
+let city4 = "jaipur"
+let q3 = city4.includes('jai')
+let q4 = city4.includes('j')
+let q5 = city4.includes('J')
+console.log(q3)
+console.log(q4)
+console.log(q5)
+
+
+// startswith()
+
+let city5 = "pune"
+let q6 = city5.startsWith('p')
+let q7 = city5.startsWith('pun')
+console.log(q6)
+console.log(q7)
+
+
+// endswith()
+let city6 = "raipur"
+let q8 = city6.endsWith('r')
+let q9 = city6.endsWith('ur')
+let q10 = city6.endsWith('Ur')
+console.log(q8)
+console.log(q9)
+console.log(q10)
+
+
+// trim()
+let c1 = " goa "
+console.log(c1.length)
+let c1a = c1.trim()
+console.log(c1a.length)
+
+// trimStart()
+let c2 = " goa "
+console.log(c2.length)
+let c2b = c2.trimStart()
+console.log(c2b.length)
+
+// trimEnd()
+let c3 = "nagpur "
+console.log(c3.length)
+let c3c = c3.trimEnd()
+console.log(c3c.length)
+
+//Split()
+
+let info = ["chinmay","gmail.com"]
+let q11 = info.join('-')
+console.log(q11)
+console.log(q11.split('-'))
+
+let city9 = 'chandrapur'
+// ["ch","ndr","pur"]
+console.log(city9.split('a'))
+
+//slice()
+
+let city10 =  "nagpur"
+
+// 0    1    2   3   4   5
+// n    a    g   p   u   r
+// -6  -5   -4  -3  -2   -1
+
+console.log(city10.slice(2))
+console.log(city10.slice(2,5))
+console.log(city10.slice(-3))
+console.log(city10.slice(-6,-3))
+console.log(city10.slice(-6,4))
+console.log(city10.slice(0,-1))
+console.log(city10.slice(-1,-3))
+
 
 
 
