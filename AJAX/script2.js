996680d1cfa3c0bf1de933a25f9390234b69386f
unknown <Minskoleopc>
2024-11-16 20:27:30 +0530

    added

--- AJAX/script2.js
+++ AJAX/script2.js
@@ -2,6 +2,8 @@
 //     console.log("A is executed")
 // }
 
+const { resolvePath } = require("react-router-dom")
+
 // function additionB(){
 //     console.log("B is executed")
 // }
@@ -60,19 +62,176 @@ function getInfo() {
 
 // pending  , resolve and reject 
 
-let pro = new Promise(function(resolve,reject){
-    let a = 10
-    let b = 1
-    if(a == b){
-        resolve("hello")
+// let pro = new Promise(function(resolve,reject){
+//     let a = 10
+//     let b = 1
+//     if(a == b){
+//         resolve("hello")
+//     }
+//     else {
+//         reject("bye")
+//     }
+// })
+
+// pro.then(function(str){
+//     console.log(str)
+// },function(str){
+//     console.log(str)
+// })
+
+// day 2
+
+
+let pro2  = new Promise(function(resolve,reject){
+    let city = "Pune"
+    if(city.includes('p')){
+        resolve([11,22,33])
     }
     else {
-        reject("bye")
+        reject([-11,-12,-13])
     }
 })
 
-pro.then(function(str){
+pro2.then(function(arr){
+    console.log(arr[0])
+},function(arr){
+    console.log(arr[1])
+})
+
+// program 2
+
+
+let pro3 = new Promise(function(resolve,reject){
+    let a = 10
+    let b = 10
+    if(a == b){
+        resolve("solved")
+    }
+    else {
+        reject("unsolved")
+    }
+
+})
+
+pro3
+.then(function(str){
     console.log(str)
-},function(str){
+})
+.catch(function(str){
     console.log(str)
+})
+
+// program 4
+
+// let pro4 = new Promise(function(resolve,reject){
+//     let firstName = "chinmy"
+//     if(firstName.length >= 7){
+//         resolve("name is greater or equal  7 characters")
+//     }
+//     else {
+//         reject('name is not greater then 7 characters')
+//     }
+
+// })
+
+// pro4
+// .then(function(str){
+//     console.log(str)
+// })
+// .catch(function(str){
+//     console.log(str)
+// })
+// .finally(function(){
+//     console.log("i will always execute")
+// })
+
+
+// program 5
+
+// let proB = new Promise(function(resolve,reject){
+//     let a = 10
+//     let b = 10
+//     if(a == b){
+//         resolve("equal")
+//     }
+//     else {
+//         reject("not equal")
+//     }
+
+// })
+
+// proB
+// .then(function(str){
+//     console.log(str)
+//     return "hello"
+// })
+// .then(function(str){
+//     console.log(str)
+// })
+// .catch(function(str){
+//     console.log(str)
+// })
+// .finally(function(){
+//     console.log("i will always execute..")
+// })
+
+
+
+
+// Promblem ----
+
+// asyn functions ------> sync
+function getInfo() {
+    setTimeout(function () {
+        console.log("user created")
+        setTimeout(function () {
+            console.log("get id")
+            setTimeout(function () {
+                console.log("get info")
+            }, 1000)
+        }, 2000)
+    }, 3000)
+}
+//getInfo()
+
+function createUser(){
+    return new Promise(function(resolve,reject){
+        setTimeout(function(){
+            resolve("user created....")
+        },3000)
+    })
+}
+function getid(){
+    return new Promise(function(resolve,reject){
+        setTimeout(function(){
+            resolve("get id....")
+        },2000)
+    })
+}
+
+function getInfo(){
+    return new Promise(function(resolve,reject){
+        setTimeout(function(){
+            resolve("get info....")
+        },1000)
+    })
+}
+
+createUser()
+.then(function(str){
+    console.log(str)
+    return getid()
+})
+.then(function(str){
+    console.log(str)
+    return getInfo()
+})
+.then(function(str){
+    console.log(str)
+})
+.catch(function(){
+    console.log("error")
+})
+.finally(function(){
+    console.log("always executed...")
 })
